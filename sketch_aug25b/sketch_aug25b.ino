91206d5f85675a514eafb6b1765cd1f7245969cf
Markus <makke00694@gmail.com>
2022-01-02 17:02:00 +0200

    added pins and variables for encoders

--- sketch_aug25b/sketch_aug25b.ino
+++ sketch_aug25b/sketch_aug25b.ino
@@ -4,14 +4,19 @@ const int vMes = A0;   // Voltage measurement from opamp 2-4V
 const int cMes = A1;   // Current measurement from opamp 2-4V
 const int cA = 2;      // Current rotary encoder pin A
 const int cB = 4;      // Current rotary encoder pin B
+int caState;           // Current rotary encoder pin A state
+int cbState;           // Current rotary encoder pin B state
 const int vA = 5;      // Voltage rotary encoder pin A
 const int vB = 6;      // Voltage rotary encoder pin B
+int vaState;           // Voltage rotary encoder pin A state
+int vbState;           // Voltage rotary encoder pin B state
 const int ocp = 7;     // Overcurrent protection
 const int pLed = 8;    // Power led
 const int outSw = 12;  // Output switch
 uint16_t currentVal = 0; // Current value read from Cmes. 10-bit
 uint16_t currentLim = 0; // Current limit set by encoder. 10-bit
 uint16_t voltageLim = 0; // Voltage limit set by encoder. 10-bot
+uint8_t count = 0;
 
 void setup() {
   // pinmodes of every I/O
@@ -24,6 +29,10 @@ void setup() {
   pinMode(ocp, OUTPUT);
   pinMode(pLed, OUTPUT);
   pinMode(outSw, INPUT);
+  pinMode(caState, INPUT_PULLUP);
+  pinMode(cbState, INPUT_PULLUP);
+  pinMode(vaState, INPUT_PULLUP);
+  pinMode(vbState, INPUT_PULLUP);
   Serial.begin(115200);
 }
 
@@ -41,19 +50,15 @@ int setVoltageLimit(voltageLim){  // Function will set the voltage limit
 
 
 void loop() {
-  currentLimit_10bit = setCurrentLimit(); // Read the current limit
-  currentVal_10bit = analogRead(cMes);  // Read the current value           
-  Serial.print(currentVal);
-  Serial.print("\t");
-  Serial.println(currentLimit_10bit);
+  currentLimit = setCurrentLimit(); // Read the current limit
+  currentVal = analogRead(cMes);  // Read the current value           
+ 
 
-  while (currentVal_10bit >= currentLimit_10bit) {  // Compare value and limit
+  while (currentVal >= currentLimit) {  // Compare value and limit
     digitalWrite(ocp, HIGH);            // Set ocp high if value >= limit
-    currentVal_10bit = analogRead(cMes);
-    currentLimit = 650;
-    Serial.print(currentVal);
-    Serial.print("\t");
-    Serial.println(currentLimit);
+    currentVal = analogRead(cMes);
+    currentLimit = setCurrentLimit();
+  
   }
 
   digitalWrite(ocp, LOW);
