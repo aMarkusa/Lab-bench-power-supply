1a8d40d654c2edc6d91b4e7da19462711abc7887
Markus <makke00694@gmail.com>
2022-01-08 16:29:17 +0200

    Added voltage meas and pled function

--- sketch_aug25b/sketch_aug25b.ino
+++ sketch_aug25b/sketch_aug25b.ino
@@ -1,21 +1,26 @@
 // program used to control the voltage and current of the psu
 
-const int vMes = A0;   // Voltage measurement from opamp 2-4V
-const int cMes = A1;   // Current measurement from opamp 2-4V
-const int cA = 2;      // Current rotary encoder pin A
-const int cB = 4;      // Current rotary encoder pin B
+// pin definition
+#define vMes A0   // Voltage measurement from opamp 2-4V
+#define cMes A1   // Current measurement from opamp 2-4V
+#define Vset 3    // voltage set signal going to LT1085
+#define cA 2      // Current rotary encoder pin A
+#define cB 4      // Current rotary encoder pin B
+#define vA 5      // Voltage rotary encoder pin A
+#define vB 6      // Voltage rotary encoder pin B
+#define ocp 7     // Overcurrent protection
+#define pLed 8    // Power led
+#define outSw 12  // Output switch
+
+// value definition
 int caState;           // Current rotary encoder pin A state
 int cbState;           // Current rotary encoder pin B state
-const int vA = 5;      // Voltage rotary encoder pin A
-const int vB = 6;      // Voltage rotary encoder pin B
 int vaState;           // Voltage rotary encoder pin A state
 int vbState;           // Voltage rotary encoder pin B state
-const int ocp = 7;     // Overcurrent protection
-const int pLed = 8;    // Power led
-const int outSw = 12;  // Output switch
 uint16_t currentVal = 0; // Current value read from Cmes. 10-bit
 uint16_t currentLim = 0; // Current limit set by encoder. 10-bit
 uint16_t voltage = 0; // Voltage limit set by encoder. 10-bit
+uint16_t voltageMesVal = 0;  // Measured voltage value from voltage divider 
 uint8_t count = 0;
 
 void setup() {
@@ -33,57 +38,61 @@ void setup() {
   pinMode(cbState, INPUT_PULLUP);
   pinMode(vaState, INPUT_PULLUP);
   pinMode(vbState, INPUT_PULLUP);
-  Serial.begin(115200);
+
   // read states of encoders
   caState = digitalRead(cA);
   cbState = digitalRead(cB);
   vaState = digitalRead(vA);
   vbState = digitalRead(vB);
-  // set current and voltage to 0
+
+   Serial.begin(115200);
 }
 
 void setCurrentLimit(){  // Function will set the current limit value
   int newStateA = digitalRead(cA);
   int newStateB = digitalRead(cB);
-    if(newStateB != caState) {
+    if(newStateB != caState) {  // If-statement for adjusting currentlim value
       if(newStateB != cbState) 
         currentLim ++;
       else
         currentLim --;
     }
-  if(currentLim > 255) 
+  if(currentLim > 255)  // Value cant be over 255
     currentLim = 255;
 }
 
 void setVoltage(){  // Function will set the voltage limit
   int newStateA = digitalRead(vA);
   int newStateB = digitalRead(vB);
-    if(newStateB != vaState) {
+    if(newStateB != vaState) {  // If-statement for adjusting voltage value
       if(newStateB != vbState) 
         voltage ++;
       else
         voltage --;
     }
-  if(voltage > 255) 
+  if(voltage > 255)  // Value cant be over 255
     voltage = 255;
   analogWrite(Vset, voltage);
 }
 
-
+float measureVoltage(){
+  voltageMesVal = 15/1023 * analogRead(vMes);  // 10-bit voltage value(1023 = 15V)
+  return roundf(voltageMesVal * 100) / 100;  // return voltage with 2 decimals
+}
 
 void loop() {
+  if(digitalRead(outSw) == 1)  // Check if output is high
+    digitalWrite(pLed, HIGH);  // power led on
+  else
+    digitalWrite(pLed, LOW);  // poer led off
   setCurrentLimit(); // Read the current limit
-  setVoltageLimit();
+  setVoltage();  
   currentVal = analogRead(cMes);  // Read the current value 
   if(currentVal > currentLim){  // Compare the current and the limit
-    digitalWrite(ocp, HIGH); // Limit the current iv val > limit
+    digitalWrite(ocp, HIGH); // Limit the current if val > limit
   }
   else{
     digitalWrite(ocp, LOW);
   }          
  
-
- 
-
-  digitalWrite(ocp, LOW);
 }
